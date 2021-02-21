3bc34b76e5e8f3c9e0bd58b62c02e336e1003ead
jordan-wong <61242306+jordan-wong@users.noreply.github.com>
2021-02-21 09:49:24 -0500

    Render movie search items in TouchableOpacity

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 import Header from './components/Header';
 import MovieListItem from './components/MovieListItem';
 import SearchBar from './components/SearchBar';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [movieItems, setMovieItems] = useState([]);
 
   useEffect(() => {
 
   }, [])
+  function Item ({id}) {
+    return (
+      <TouchableOpacity 
+        onPress={() =>{
+          console.log(id, 'was pressed');
+      }}>
+        <View style={styles.item}>
+          <Text>{id}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+    }
 
 
   return (
     <View style={styles.container}>
       <Header />
       <SearchBar setMovies={setMovies} />
-      <FlatList
+      <FlatList onPress={() => navigate(DetailsScreen)
+
+      }
         data={movies}
         renderItem={({ item }) => <MovieListItem movie={item} />}
       />
